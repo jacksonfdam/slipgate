de3cb4893f3b27e9a41f0b5ed1dc3fdfeb5275b3
Jackson F. de A. Mafra <jacksonfdam@gmail.com>
2026-08-18 22:05:50 +0200

    build(graphics-classic): add classic backend module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,6 +19,7 @@ dependencyResolutionManagement {
 
 rootProject.name = "slipgate"
 
+include(":host:graphics:backend:classic")
 include(":host:graphics:core")
 include(":host:runtime")
 include(":ui")
