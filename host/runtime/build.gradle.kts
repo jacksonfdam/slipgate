abc5cd2043ef93eb0679dfdef2458fea3c396458
Jackson F. de A. Mafra <jacksonfdam@gmail.com>
2026-08-18 21:03:19 +0200

    build(host-runtime): add host runtime module

---
+++ host/runtime/build.gradle.kts
@@ -0,0 +1,35 @@
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.kmp.library)
+}
+
+val slipgateJvmToolchain = property("slipgate.jvmToolchain") as String
+val slipgateAndroidCompileSdk = property("slipgate.androidCompileSdk") as String
+val slipgateAndroidMinSdk = property("slipgate.androidMinSdk") as String
+
+kotlin {
+    explicitApi()
+    jvmToolchain(slipgateJvmToolchain.toInt())
+
+    android {
+        namespace = "com.jacksonfdam.slipgate.host.runtime"
+        compileSdk = slipgateAndroidCompileSdk.toInt()
+        minSdk = slipgateAndroidMinSdk.toInt()
+    }
+
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser()
+    }
+
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.koin.core)
+        }
+    }
+}
