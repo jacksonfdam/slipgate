23d466aa8d23cc48bb14fffe10ec738c9f022ef5
Jackson F. de A. Mafra <jacksonfdam@gmail.com>
2026-08-19 09:41:23 +0200

    build(game-data): add the game data module

---
+++ host/gamedata/build.gradle.kts
@@ -0,0 +1,48 @@
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.kmp.library)
+}
+
+val slipgateJvmToolchain = property("slipgate.jvmToolchain") as String
+val slipgateAndroidCompileSdk = property("slipgate.androidCompileSdk") as String
+val slipgateAndroidMinSdk = property("slipgate.androidMinSdk") as String
+
+// Lets a real IWAD be pointed at the inspector without one ever living in the repository.
+val suppliedIwad = providers.gradleProperty("slipgate.iwad")
+
+tasks.withType<Test>().configureEach {
+    environment("SLIPGATE_IWAD", suppliedIwad.getOrElse(""))
+}
+
+kotlin {
+    explicitApi()
+    jvmToolchain(slipgateJvmToolchain.toInt())
+
+    android {
+        namespace = "com.jacksonfdam.slipgate.host.gamedata"
+        compileSdk = slipgateAndroidCompileSdk.toInt()
+        minSdk = slipgateAndroidMinSdk.toInt()
+    }
+
+    jvm()
+
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser()
+    }
+
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            api(project(":host:runtime"))
+        }
+
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+    }
+}
